fd325457319aa885b8cebc7725021dc347be1060
dazeb <dbennett2911@gmail.com>
2023-07-19 09:03:19 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <UContainer class="mt-4">
+    <UCard>
+      <template #header>
+        This is the header with a <UButton variant="outline">button</UButton>
+      </template>
+
+      <p>This is the body with a <UBadge>Badge</UBadge></p>
+    </UCard>
+  </UContainer>
+</template>
